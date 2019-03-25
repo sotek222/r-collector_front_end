d2c0aca54ba03c83b8337c0ebc83bc1f945b29f2
Farhan <farhanmahmoodny@gmail.com>
2019-03-25 16:16:58 -0400

    added from back-end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const recordsUrl = 'http://localhost:3000/records'
+  const recordsUrl = "http://localhost:3000/records"
   const recordsContainer = document.querySelector('.records-container')
 
-  fetch(recordsUrl)
-  .then(res => res.json())
-  .then(json => console.log(json))
+  function fetchRecords(url) {
+    return fetch(url)
+    .then(res => res.json())
+  }
+
+  function renderRecord(record) {
+    recordsContainer.innerHTML += `
+    <div>
+      <h1>${record.title}</h1>
+      <h2>${record.artist}</h2>
+      <h3>${record.genre}</h3>
+      <img src=${record.image_url}>
+    </div>
+    `
+  }
 
 
+  function renderAllRecords(){
+    fetchRecords(recordsUrl)
+    .then(records => {
+      records.forEach(record => {
+        renderRecord(record)
+        console.log(record)
+      })
+    })
+  }
+renderAllRecords()
 
 })
