7058e47cf443eeea5fe2b1e16f792a38676b4397
Matthew Masiello <sotek222@yahoo.com>
2019-10-29 14:25:49 -0400

    add production config

---
+++ webpack.prod.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main-[contentHash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+          loader: 'url-loader'
+      }
+    ]
+  }
+}
